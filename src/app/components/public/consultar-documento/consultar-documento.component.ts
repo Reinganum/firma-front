f7a45f052383340ad7303ecfbbbe84cb7fae031e
Nicolas Catalan <nicolas.catalan@catena.cl>
2023-07-14 10:08:14 -0400

    fix

--- src/app/components/public/consultar-documento/consultar-documento.component.ts
+++ src/app/components/public/consultar-documento/consultar-documento.component.ts
@@ -23,9 +23,7 @@ export class ConsultarDocumentoComponent implements OnInit{
   }
   consultarDocumento() {
     console.log(this.formDatosUsuario.value.codigoDoc);
-    this.toaster.success("Código del documento:" + this.formDatosUsuario.value.codigoDoc, '',{
-      positionClass:'toast-bottom-right'
-  })
+    this.toaster.success("Código del documento:" + this.formDatosUsuario.value.codigoDoc);
     this.router.navigate([`private/vista/${this.formDatosUsuario.value.codigoDoc}`]);
   }
 }
