7e537fbee6db0bf31b2bdfc3e0823ea1b24d0e16
Nicolas Catalan <nicolas.catalan@catena.cl>
2023-07-20 15:27:39 -0400

    up

--- src/app/services/correos.service.ts
+++ src/app/services/correos.service.ts
@@ -1,9 +1,34 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CorreosService {
 
-  constructor() { }
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  // Recibe como parámetros de entrada
+  // email:string,
+  // asunto:string,
+  // seguimiento:string
+  notificarDocFirmado(data:any): Observable<any> {
+    const body = JSON.stringify(data);
+    const headers = {'Content-Type': 'application/x-www-form-urlencoded'};
+    return this.http.post(`${environment.API_DOMAINS.CORREOS}/correos/avisoFirmado`, body, {headers})
+  }
+
+  // Recibe como parámetros de entrada
+  // email:string,
+  // asunto:string,
+  // nombre:string
+  notificarDocAFirmar(data:any): Observable<any> {
+    const body = JSON.stringify(data);
+    const headers = {'Content-Type': 'application/x-www-form-urlencoded'};
+    return this.http.post(`${environment.API_DOMAINS.CORREOS}/correos/docAFirmar`, body, {headers})
+  }
 }
